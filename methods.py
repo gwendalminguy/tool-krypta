1016266c9f42de5eb9e4f671d6bc655feb3788c3
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-18 12:16:17 +0200

    Created module containing encryption/decryption functions

---
+++ methods.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+methods.py
+Module containing functions to encrypt and decrypt messages.
+Supported methods : ROT13 ; Caesar ; Vigenere
+"""
+
+
+def rot13(text):
+    """
+    Encrypts or decrypts a text using ROT13.
+
+    Parameters:
+    text (str): text to encrypt or decrypt
+
+    Return: the resulting string
+    """
+    result = ""
+    for char in text:
+        if 65 <= ord(char) <= 77 or 97 <= ord(char) <= 109:
+            char = chr(ord(char) + 13)
+        elif 78 <= ord(char) <= 90 or 110 <= ord(char) <= 122:
+            char = chr(ord(char) - 13)
+        result += char
+
+    return result
+
+
+def caesar(text, key, action):
+    """
+    Encrypts or decrypts a text using Caesar method.
+
+    Parameters:
+    text (str): text to encrypt or decrypt
+    key (int): key to use
+    action (int): 1 to encrypt ; -1 to decrypt
+
+    Return: the resulting string
+    """
+    key = action * (key % 26)
+    result = ""
+    for char in text:
+        if 65 <= ord(char) <= 90:
+            char = chr(ord(char) + key)
+            if ord(char) > 90:
+                char = chr(ord(char) - 26)
+            elif ord(char) < 65:
+                char = chr(ord(char) + 26)
+        elif 97 <= ord(char) <= 122:
+            char = chr(ord(char) + key)
+            if ord(char) > 122:
+                char = chr(ord(char) - 26)
+            elif ord(char) < 97:
+                char = chr(ord(char) + 26)
+        result += char
+
+    return result
+
+
+def vigenere(text, key, action):
+    """
+    Encrypts or decrypts a text using Vigenere method. (upcoming)
+
+    Parameters:
+    text (str): text to encrypt or decrypt
+    key (str): key to use
+    action (int): 1 to encrypt ; -1 to decrypt
+
+    Return: the resulting string
+    """
+    return text
